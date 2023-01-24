5ef8a47ef2e1131dcba2efadd7ab011698fdc95e
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-01-24 21:56:11 +0800

    upload pqa project

---
+++ resources/js/pages/Official.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+    <div class="bg-red-500 py-10">
+            Official Page
+
+            {{ status }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            status: {
+                type: String,
+            },
+        },
+    }
+</script>
+
+<style  scoped>
+
+</style>
