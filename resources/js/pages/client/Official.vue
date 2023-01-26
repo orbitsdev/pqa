659d439e98b8b1ac497f48a2882b4f57d652f8e9
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-01-26 20:41:19 +0800

    version 2 ui added

--- resources/js/pages/client/Official.vue
+++ resources/js/pages/client/Official.vue
@@ -1,10 +1,66 @@
 <template>
   <!-- NAVIGATION -->
 
-  <!-- <seciton> 
+  <!-- <seciton class="lg:px-20 lg:flex lg:justify-between lg:items-center lg:py-2"> 
+      <aside class="lg:flex lg:items-center">
+          <img src="/assets/pqa-logo.png" alt="" class="lg:h-16 lg:w-16 lg:mr-6">
+          <div class="lg:flex lg:items-center ">
+            <img src="/assets/dti-logo.png" alt="" class="lg:h-16 lg:w-16 lg:mr-2">
+            
+            <div class="">
+           <p class="dti-text-blue-1 lg:leading-4 lg:text-sm ">Republic of the Philippines</p>
+           <p class="dti-text-blue-1 lg:leading-4 lg:text-sm border-t-2 dti-border-blue-1 uppercase lg:font-extrabold ">Department of trade and Industry </p>
+           <p class="dti-text-blue-1 lg:leading-4 lg:text-xs">Trabaho, Negosyo, Konsyumer</p>
+
+          </div>
+          </div>
+      </aside> 
+      
       <aside>
-          <img src="/assets/pqa-log.png" alt="">
-      </aside>    
+          <p class="dti-text-blue-1 lg:text-end lg:text-sm">Philippines Standard Time:</p>
+          <p class="dti-text-blue-1 lg:text-end lg:text-sm lg:font-bold">Monday, January 23, 2023 | 10:00:01 AM</p>
+          <ul class=" dti-text-blue-1 lg:flex lg:items-center lg:justify-end">
+            <li class="pl-3">
+              <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2699)">
+                <path d="M11.6666 12.2445H13.7499L14.5833 8.91113H11.6666V7.24447C11.6666 6.38613 11.6666 5.5778 13.3333 5.5778H14.5833V2.7778C14.3116 2.74197 13.2858 2.66113 12.2024 2.66113C9.93992 2.66113 8.33325 4.04197 8.33325 6.5778V8.91113H5.83325V12.2445H8.33325V19.3278H11.6666V12.2445Z" fill="#233A84"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2699">
+                <rect width="20" height="20" fill="#233A84" transform="translate(0 0.994629)"/>
+                </clipPath>
+                </defs>
+                </svg>
+                
+            </li>
+            <li class="pl-3">
+              <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2702)">
+                <path d="M18.9683 5.7079C18.3321 5.98931 17.6574 6.17413 16.9666 6.25623C17.6947 5.82076 18.2397 5.13541 18.4999 4.3279C17.8166 4.73457 17.0674 5.01957 16.2866 5.17373C15.7621 4.61255 15.0669 4.24037 14.3091 4.11506C13.5512 3.98974 12.7732 4.11831 12.096 4.48078C11.4187 4.84325 10.8802 5.4193 10.5642 6.11939C10.2481 6.81949 10.1722 7.60438 10.3483 8.35207C8.96251 8.28261 7.60687 7.9225 6.36933 7.29512C5.13179 6.66774 4.04003 5.78711 3.16492 4.7104C2.85516 5.24244 2.69238 5.84726 2.69326 6.4629C2.69326 7.67123 3.30826 8.73873 4.24326 9.36373C3.68993 9.34631 3.14878 9.19689 2.66492 8.9279V8.97123C2.66509 9.77598 2.94356 10.5559 3.45313 11.1788C3.96269 11.8017 4.67199 12.2291 5.46076 12.3887C4.9471 12.5279 4.40851 12.5484 3.88576 12.4487C4.10814 13.1414 4.54159 13.7472 5.12542 14.1813C5.70924 14.6154 6.4142 14.8561 7.14159 14.8696C6.41866 15.4373 5.5909 15.8571 4.70566 16.1047C3.82041 16.3524 2.89503 16.4231 1.98242 16.3129C3.5755 17.3374 5.43 17.8813 7.32409 17.8796C13.7349 17.8796 17.2408 12.5687 17.2408 7.9629C17.2408 7.8129 17.2366 7.66123 17.2299 7.5129C17.9123 7.01971 18.5013 6.40875 18.9691 5.70873L18.9683 5.7079Z" fill="#233A84"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2702">
+                <rect width="20" height="20" fill="#233A84" transform="translate(0.5 0.994629)"/>
+                </clipPath>
+                </defs>
+                </svg>
+                
+            </li>
+            <li class="pl-3">
+              <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1_2705)">
+                <path d="M15.2792 16.2771H13.0583V12.7963C13.0583 11.9663 13.0417 10.898 11.9 10.898C10.7425 10.898 10.5658 11.8013 10.5658 12.7355V16.2771H8.34417V9.11963H10.4775V10.0946H10.5067C10.805 9.53296 11.53 8.9388 12.6133 8.9388C14.8633 8.9388 15.28 10.4205 15.28 12.348V16.2771H15.2792ZM5.83583 8.14046C5.6663 8.14068 5.49838 8.10744 5.34172 8.04263C5.18506 7.97783 5.04272 7.88274 4.92288 7.76283C4.80304 7.64291 4.70804 7.50051 4.64334 7.34381C4.57864 7.1871 4.5455 7.01917 4.54583 6.84963C4.546 6.59449 4.62182 6.34513 4.7637 6.13308C4.90558 5.92104 5.10716 5.75582 5.34294 5.65834C5.57872 5.56085 5.83811 5.53548 6.08832 5.58541C6.33852 5.63535 6.5683 5.75836 6.74859 5.93888C6.92888 6.11941 7.05159 6.34935 7.10121 6.59961C7.15082 6.84988 7.12511 7.10924 7.02732 7.34489C6.92953 7.58055 6.76406 7.78191 6.55183 7.92352C6.33959 8.06513 6.09014 8.14063 5.835 8.14046H5.83583ZM6.94917 16.2771H4.72167V9.11963H6.95V16.2771H6.94917ZM16.3917 3.49463H3.6075C2.99417 3.49463 2.5 3.97796 2.5 4.57546V17.4138C2.5 18.0113 2.995 18.4946 3.60667 18.4946H16.3883C17 18.4946 17.5 18.0113 17.5 17.4138V4.57546C17.5 3.97796 17 3.49463 16.3883 3.49463H16.3908H16.3917Z" fill="#233A84"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1_2705">
+                <rect width="20" height="20" fill="#233A84" transform="translate(0 0.994629)"/>
+                </clipPath>
+                </defs>
+                </svg>
+                
+            </li>
+          </ul>
+      </aside>
   </seciton> -->
   <section class="z-10 pqa-blue-1 px-20 relative">
     <aside class="">
@@ -21,9 +77,9 @@
       </ul>
     </aside>
 
-    <aside class="pqa-blue-linear flex justify-between items-center">
-      <p class="text-white font-play-fair lg:text-xl lg:font-bold lg:ml-20">
-        Philippine Quality Award
+    <aside class="pqa-blue-linear flex justify-between items-center shadow">
+      <p class="font-raleway text-white font-play-fair lg:text-xl lg:font-bold lg:ml-20">
+      GOVEPH
       </p>
       <ul class="flex items-center">
         <li
@@ -80,7 +136,7 @@
         <li
           class="font-raleway text-white uppercase lg:text-sm lg:ml-2 lg:py-4 lg:px-3 cursor-pointer pqa-nav-text"
         >
-          contact ise
+          contact
         </li>
         <li
           class="font-raleway text-white uppercase lg:text-sm lg:ml-2 lg:py-4 lg:px-3 cursor-pointer pqa-nav-text"
@@ -100,6 +156,11 @@
             />
           </svg>
         </li>
+        <li
+        class="font-raleway text-white uppercase lg:text-sm lg:ml-2 lg:py-4 lg:px-3 cursor-pointer pqa-nav-text"
+      >
+        Login
+      </li>
       </ul>
     </aside>
 
